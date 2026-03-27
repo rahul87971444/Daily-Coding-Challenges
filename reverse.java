1aae6a8d2724c3ebdde0ab8fa366bfb1d0769268
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-03-27 09:45:34 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,12 @@
+public class reverse {
+    public static void main(String args[]){
+        String s="2345";
+        String r=" ";
+        for (int i=s.length()-1;i>=0;i--){
+            r=r+s.charAt(i);
+        }
+        System.out.print(r);
+
+    }
+    
+}
