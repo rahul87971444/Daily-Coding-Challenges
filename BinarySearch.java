62eddff9bc468c4da57095656cd2e9e8139b0f75
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-03-28 20:48:49 +0530

    Implement binary search code

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,1 +1,29 @@
+public class BinarySearch {
+    public static void main(String args[]){
+        int arr[]={-2,4,5,36,40};
+        int target=5;
+        int ans=binarys(arr,target);
+        System.out.println(ans);
 
+    }
+    static int binarys(int arr[],int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target<arr[mid]){
+                end=mid-1;
+            }
+            else if(target>arr[mid]){
+                start=mid+1;
+
+            }
+            else{
+                return mid;
+            }
+        }
+            return -1;
+    }
+    
+    
+}
