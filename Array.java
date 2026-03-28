aa254c5a95259f829832906c84f8b725837e82ac
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-03-28 20:50:03 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 44;
+        arr[1]  = 444;     
+        arr[2] = 567;
+        arr[3] = 1233;
+         arr[4] = 5678;
+         System.out.println(arr[2]);
+    }
+}
