08e644c04bdeaea6c93ff94253ed12250de8b3d9
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-05-08 23:45:52 +0530

    Implement ContainerWithMostWater code

---
+++ Problem:ContainerWithMostWater/ContainerWithMostWater.java
@@ -0,0 +1,32 @@
+class ContainerWithMostWater {
+    public static int MaxWater(int height[]) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+        while (left < right) {
+            int width = right - left;
+
+          // OR int currentWater = Math.min(height[left], height[right]) * width;
+                //maxWater = Math.max(maxWater, currentWater);
+          
+            int h = Math.min(height[left], height[right]);
+            int area = width * h;
+            maxWater = Math.max(maxArea, area);
+            // Move smaller height pointer
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+
+    public static void main(String args[]) {
+
+        int height[] = {1,8,6,2,5,4,8,3,7};
+
+        System.out.println(MaxWater(height));
+    }
+}
