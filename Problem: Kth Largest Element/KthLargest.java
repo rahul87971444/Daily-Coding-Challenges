df0b3dd18e666d3380ae9258c1c0f756b980f0ad
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-04 01:54:05 +0530

    Implement KthLargest class to find kth largest element

---
+++ Problem: Kth Largest Element/KthLargest.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+public class KthLargest {
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        System.out.println(pq.peek());
+    }
+}
